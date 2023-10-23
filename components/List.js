170c691725b07747a4dd6afbe66972d65a1fd3e6
“Zane <zlh4@duke.edu>
2023-10-23 12:33:32 -0400

    Made list narrower, added list data template

--- components/List.js
+++ components/List.js
@@ -1,5 +1,12 @@
 import styles from "../styles/list.module.css";
 
 export default function List() {
+  const items = [
+    {
+      name: "Item name",
+      quantity: "Amount",
+    },
+  ];
+
   return <div className={styles.list}></div>;
 }
