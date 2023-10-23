29af6523b8029dfe4ac30def4876361984064c90
“Zane <zlh4@duke.edu>
2023-10-23 12:28:27 -0400

    Made list more narrow, centered list items

--- components/ListItem.js
+++ components/ListItem.js
@@ -3,7 +3,7 @@ import styles from "../styles/list-item.module.css";
 export default function ListItem({ name, quantity }) {
   return (
     <div className={styles["list-item"]}>
-      <p className={styles["list-item-text"]}>
+      <p>
         {name}, {quantity}
       </p>
       <label className={styles["label"]}>
